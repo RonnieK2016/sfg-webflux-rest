7133aeabd555802769dc13be58131a744dc76b74
SergeA <ronnieo@gmail.com>
2019-04-08 21:51:36 -0400

    Initial commit

---
+++ src/main/java/com/udemy/spring5/sfgwebfluxrest/SfgWebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring5.sfgwebfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgWebfluxRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgWebfluxRestApplication.class, args);
+	}
+
+}
